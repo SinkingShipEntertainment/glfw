dac7986039afe25b825a23d198cf5b132b36ab4a
Marcelo Sercheli <msercheli@gmail.com>
2023-06-05 10:26:05 -0400

    Modify package

--- package.py
+++ package.py
@@ -13,26 +13,13 @@ description = \
     """
 
 with scope("config") as c:
-    # Determine location to release: internal (int) vs external (ext)
-
-    # NOTE: Modify this variable to reflect the current package situation
-    release_as = "ext"
-
-    # The `c` variable here is actually rezconfig.py
-    # `release_packages_path` is a variable defined inside rezconfig.py
-
     import os
-    if release_as == "int":
-        c.release_packages_path = os.environ["SSE_REZ_REPO_RELEASE_INT"]
-    elif release_as == "ext":
-        c.release_packages_path = os.environ["SSE_REZ_REPO_RELEASE_EXT"]
+    c.release_packages_path = os.environ["SSE_REZ_REPO_RELEASE_EXT"]
 
 requires = [
 ]
 
 private_build_requires = [
-    "cmake",
-    "gcc",
 ]
 
 variants = [
@@ -40,8 +27,10 @@ variants = [
 ]
 
 build_system = "cmake"
-
 uuid = "repository.glfw"
+
+def pre_build_commands():
+    command("source /opt/rh/devtoolset-6/enable")
 
 def commands():
     env.GLFW_ROOT = "{root}"
